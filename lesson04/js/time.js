8c5af4b0a323391415a33a94e8d1d967843ef2aa
jess.flint4@gmail.com <59670594+jessflint4@users.noreply.github.com>
2020-02-08 16:09:55 -0500

    x

--- lesson04/js/time.js
+++ lesson04/js/time.js
@@ -1,6 +1,7 @@
-<<<<<<< HEAD
-const options={hour: 'numeric', minute: 'numeric',weekday:'long', day: 'numeric', month: 'long', year:'numeric'};
-=======
-const options={hour: 'numeric', minute: 'numeric',weekday:'long', day: 'numeric', month: 'long', year:'numeric'};
->>>>>>> ded899524b223723093b75aa54633e84d6917694
-document.getElementById('upDate').textContent= new Date().toLocaleDateString('en-US', options);
+var whatdate  = new Date();
+
+if (whatdate.getDay() == 5 ) {
+  document.querySelector("aside").style.display = "block";
+} else {
+  document.querySelector("aside").style.display = "none";
+}
